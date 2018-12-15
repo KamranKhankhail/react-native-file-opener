210a85af7a42d66a5a1896b3351a05ca0c7982b2
Macbook Pro <mac@Macbooks-MacBook-Pro.local>
2018-12-15 14:09:12 +0500

    fixed build failed

---
+++ ios/RNFileOpener/RNFileOpener.h
@@ -0,0 +1,13 @@
+#if __has_include(<React/RCTBridgeModule.h>)
+#import <React/RCTBridgeModule.h>
+#else
+#import "RCTBridgeModule.h"
+#endif
+#import "RCTBridge.h"
+
+
+@import UIKit;
+
+@interface FileOpener : NSObject <RCTBridgeModule>
+@property (nonatomic) UIDocumentInteractionController * FileOpener;
+@end
